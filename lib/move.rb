ee9859245dfec8055e882654b87ddfe20e06db98
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-10-05 15:44:48 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,11 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+board = [" ", " ", " "]
+def update_array_at_with(array, index, value)
+  array[index] = value
+end
+
+update_array_at_with(board, 0, "X")
+
+board
