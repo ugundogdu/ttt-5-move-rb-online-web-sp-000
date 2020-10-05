5fa66b637a9ccf698494c7684c0fa7804ec9d0af
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-10-05 18:50:48 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,5 +10,5 @@ def input_to_index(user_input)
   user_input = "1"
   converted_input = input_to_index(user_input)
 
-  expect(converted_input).to be_a(Integer)
+  expect(converted_input).to be_a("1" - "9")
 end
