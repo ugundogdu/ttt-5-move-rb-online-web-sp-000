255cee733ec9da772b7cf9b2ae7e8afe34046b09
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-10-05 18:41:04 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,11 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board = [" ", " ", " "]
-def update_array_at_with(array, index, value)
-  array[index] = value
+def input_to_index(user_input)
+  user_input = ("1 - "9)
 end
-
-update_array_at_with(board, 0, "X")
-
-board
