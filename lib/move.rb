f7365371bf661a37f89a4bd2a0ebb27315205d66
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-10-05 18:23:59 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,3 +5,11 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index(input)
+  input = index.to_i - 1
+end
+
+def move(board, index, user_marker = "X")
+  board[index.to_i] = user_marker
+end
